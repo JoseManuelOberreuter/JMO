c7e5a92c0a088fc955d3a1302447f99c1e67b013
Transformer1997 <jose.manuel.oberreuter@gmail.com>
2023-02-09 11:21:03 -0300

    some changes

---
+++ js/palabras.js
@@ -0,0 +1,26 @@
+const words = ['Estudiante de Ingienería Informatica', 'Desarrollador web FreeLancer', 'Full Stack developer'];
+  let i = 0;
+  let j = 0;
+  let isDeleting = false;
+  
+  setInterval(() => {
+    const word = words[i];
+    let text = document.querySelector('#text').innerHTML;
+    
+    if (isDeleting) {
+      text = text.substring(0, text.length - 1);
+      if (text === '') {
+        isDeleting = false;
+        i = (i + 1) % words.length;
+        j = 0;
+      }
+    } else {
+      text = word.substring(0, j + 1);
+      j++;
+      if (j === word.length) {
+        isDeleting = true;
+      }
+    }
+    
+    document.querySelector('#text').innerHTML = text;
+  }, 150);
