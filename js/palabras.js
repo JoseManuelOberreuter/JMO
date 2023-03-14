44472124010e735c6ba36465e814851b0674dd32
Transformer1997 <jose.manuel.oberreuter@gmail.com>
2023-03-14 19:57:21 -0300

    update index
    -update button item4
    -color footer
    -adding social media to the menu

--- js/palabras.js
+++ js/palabras.js
@@ -1,4 +1,4 @@
-const words = ['Full Stack developer','Desarrollador Web FreeLancer','Estudiante de Ingienería Informatica', ''];
+const words = ['Software Developer','Full Stack developer','Desarrollador Web FreeLancer','Estudiante de Ingienería Informatica'];
   let i = 0;
   let j = 0;
   let isDeleting = false;
