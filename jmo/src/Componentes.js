2e4aaccd512669000f91a29c2625ef992e9229f2
Transformer1997 <jose.manuel.oberreuter@gmail.com>
2023-10-04 21:58:08 -0300

    footer update

--- jmo/src/Componentes.js
+++ jmo/src/Componentes.js
@@ -18,7 +18,7 @@ export const Header = () => {
             <IconosRS />
 
             <div class="col-4 text-center">
-                <h2>BLOG JMO</h2>
+                <h2>Blog de programación</h2>
             </div>
 
             <SearchBar />
@@ -454,48 +454,28 @@ export function Footer() {
   return (
     <div className="container border-top">
       <footer className="py-5">
-        <div className="row">
+        <div className="row ">
+
+
           <div className="col-6 col-md-2 mb-3">
-            <h5>Section</h5>
+            <h5>JMO</h5>
             <ul className="nav flex-column">
-              <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-body-secondary">Home</a></li>
-              <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-body-secondary">Features</a></li>
-              <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-body-secondary">Pricing</a></li>
-              <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-body-secondary">FAQs</a></li>
-              <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-body-secondary">About</a></li>
+              <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-body-secondary">Blog</a></li>
+              <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-body-secondary">Portafolio</a></li>
+              <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-body-secondary">Sobre mi</a></li>
             </ul>
           </div>
 
-          <div className="col-6 col-md-2 mb-3">
-            <h5>Section</h5>
-            <ul className="nav flex-column">
-              <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-body-secondary">Home</a></li>
-              <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-body-secondary">Features</a></li>
-              <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-body-secondary">Pricing</a></li>
-              <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-body-secondary">FAQs</a></li>
-              <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-body-secondary">About</a></li>
-            </ul>
-          </div>
 
-          <div className="col-6 col-md-2 mb-3">
-            <h5>Section</h5>
-            <ul className="nav flex-column">
-              <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-body-secondary">Home</a></li>
-              <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-body-secondary">Features</a></li>
-              <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-body-secondary">Pricing</a></li>
-              <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-body-secondary">FAQs</a></li>
-              <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-body-secondary">About</a></li>
-            </ul>
-          </div>
 
           <div className="col-md-5 offset-md-1 mb-3">
             <form>
-              <h5>Subscribe to our newsletter</h5>
-              <p>Monthly digest of what's new and exciting from us.</p>
+              <h5>Comunícate Conmigo</h5>
+              <p>Por favor, ingresa tu consulta a continuación y te responderé a la brevedad.</p>
               <div className="d-flex flex-column flex-sm-row w-100 gap-2">
-                <label htmlFor="newsletter1" className="visually-hidden">Email address</label>
-                <input id="newsletter1" type="text" className="form-control" placeholder="Email address" />
-                <button className="btn btn-primary" type="button">Subscribe</button>
+                <label htmlFor="newsletter1" className="visually-hidden">Escríbeme un mensaje</label>
+                <input id="newsletter1" type="text" className="form-control" placeholder="Escríbeme un mensaje" />
+                <button className="btn btn-primary" type="button">Enviar</button>
               </div>
             </form>
           </div>
