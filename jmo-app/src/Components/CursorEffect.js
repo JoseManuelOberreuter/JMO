8ae0783c9ca9cf00d7f1c8497320f1e5c7336ece
jmo-bancaeticalat <101443468+JoseManuelOberreuter@users.noreply.github.com>
2024-08-22 22:12:12 -0400

    Update CursorEffect

--- jmo-app/src/Components/CursorEffect.js
+++ jmo-app/src/Components/CursorEffect.js
@@ -8,9 +8,8 @@ export const CursorEffect = () => {
     const coords = { x: 0, y: 0 };
     const colors = [
       '#868686', '#7b7b7b', '#707070', '#656565', '#5b5b5b', '#515151', '#464646', '#3d3d3d', '#333333', '#2a2a2a', '#212121', '#181818'
-  ];
+    ];
 
-    // Calculate color step to apply color linearly across all circles
     circleRefs.current.forEach((circle, index) => {
       const colorIndex = Math.floor((index / (circleRefs.current.length - 1)) * (colors.length - 1));
       circle.style.backgroundColor = colors[colorIndex];
@@ -28,16 +27,9 @@ export const CursorEffect = () => {
     const animateCircles = () => {
       let x = coords.x;
       let y = coords.y;
-
-      const isOverLink = document.elementFromPoint(x, y)?.tagName.toLowerCase() === 'a';
-      const isOverImg = document.elementFromPoint(x, y)?.tagName.toLowerCase() === 'img';
-      const isOverInput = document.elementFromPoint(x, y)?.tagName.toLowerCase() === 'input';
-      const isOverTextArea= document.elementFromPoint(x, y)?.tagName.toLowerCase() === 'textarea';
-
       circleRefs.current.forEach((circle, index) => {
-        if (isOverLink || isOverImg || isOverInput || isOverTextArea) {
-          circle.style.display = 'none';
-        } else {
+ 
+          circle.classList.remove('shrink');
           circle.style.display = 'block';
           circle.style.left = x - 12 + "px";
           circle.style.top = y - 12 + "px";
@@ -49,7 +41,7 @@ export const CursorEffect = () => {
           const nextCircle = circleRefs.current[index + 1] || circleRefs.current[0];
           x += (nextCircle.x - x) * 0.3;
           y += (nextCircle.y - y) * 0.3;
-        }
+        
       });
 
       requestAnimationFrame(animateCircles);
@@ -57,7 +49,6 @@ export const CursorEffect = () => {
 
     animateCircles();
 
-    // Cleanup on component unmount
     return () => {
       window.removeEventListener("mousemove", handleMouseMove);
     };
