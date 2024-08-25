f1246048b6587cb637f25f2de9f189d63293d3bc
jmo-bancaeticalat <101443468+JoseManuelOberreuter@users.noreply.github.com>
2024-08-25 11:41:15 -0400

    Update About Me

--- jmo-app/src/Components/ItemAboutMe.js
+++ jmo-app/src/Components/ItemAboutMe.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import imgYo from './img/about/yo.jpg'
+import imgYo from './img/about/yo.jpg';
+import '../Css/AboutMe.css'; // Asegúrate de incluir el archivo CSS
 
-
-  export const ItemAboutMe = () => {
-      return (
-        <div className="col-xxl-8">
+export const ItemAboutMe = () => {
+    return (
+        <div className="container px-4 py-5">
             <div className="row gx-5">
                 {/* Columna de la imagen */}
                 <div className="col-md-6">
@@ -13,18 +13,18 @@ import imgYo from './img/about/yo.jpg'
                 {/* Columna del texto */}
                 <div className="col-md-6 d-flex align-items-center">
                     <div className="text-left my-5">
-                        <h2 className="display-5 fw-bolder"><span className="text-gradient d-inline">JMO</span></h2>
-                        <p className="lead fw-light mb-4">Hola! Mi nombre es José Manuel Oberreuter </p>
+                        <h2 className="display-5 fw-bolder">
+                            <span className="text-gradient d-inline">JMO</span>
+                        </h2>
+                        <p className="lead fw-light mb-4">Hola! Mi nombre es José Manuel Oberreuter</p>
                         <p className="text-white">
-                            Soy un estudiante en Ingeniería Informatica, con 
-                            un enfoque destacado en la innovación. Mi habilidad para abordar
-                            desafíos se basa en mi compromiso con la autoeducación.
+                            Soy un estudiante en Ingeniería Informática, con un enfoque destacado en la innovación. 
+                            Mi habilidad para abordar desafíos se basa en mi compromiso con la autoeducación.
                             Aspiro a ser un experto en Inteligencia Artificial.
                         </p>
                     </div>
                 </div>
             </div>
         </div>
-      );
-  };
-
+    );
+};
--- jmo-app/src/Components/ItemCertificate.js
+++ jmo-app/src/Components/ItemCertificate.js
@@ -1,17 +1,20 @@
 import React from 'react';
 import certificado from './img/about/certificado_az900.jpg';
+import '../Css/AboutMe.css'; // Asegúrate de incluir el archivo CSS
 
 export const ItemCertificate = () => {
     return (
-        <div className="col-xxl-8">
-            <div className="row gx-5 "> 
+        <div className="item-certificate-container">
+            <div className="row gx-5">
                 {/* Columna del texto */}
                 <div className="col-md-6 d-flex align-items-center">
-                    <div className="my-5" style={{ textAlign: 'end'}}> {/* Agregamos la clase text-right */}
-                        <h2 className="display-5 fw-bolder"><span className="text-gradient d-inline">Nube de Microsoft</span></h2>
+                    <div className="text-section">
+                        <h2 className="display-5 fw-bolder">
+                            <span className="text-gradient d-inline">Nube de Microsoft</span>
+                        </h2>
                         <p className="lead fw-light mb-4">
-                            Certificado en Microsoft Azure, mi formación en la plataforma Azure me permite ofrecer soluciones en la nube.    
-                        </p>                           
+                            Certificado en Microsoft Azure, mi formación en la plataforma Azure me permite ofrecer soluciones en la nube.
+                        </p>
                         <p className="text-white">
                             Este certificado demuestra mis habilidades en la gestión y administración de servicios en la nube.
                         </p>
@@ -19,7 +22,7 @@ export const ItemCertificate = () => {
                 </div>
                 {/* Columna de la imagen */}
                 <div className="col-md-6 d-flex justify-content-center">
-                    <img src={certificado} alt="Certificado Microsoft AZ 900" className="rounded" style={{ width: '200%', height: '400px', marginLeft: '150px' }} /> {/* Agrandamos la imagen */}
+                    <img src={certificado} alt="Certificado Microsoft AZ 900" className="certificado-img" />
                 </div>
             </div>
         </div>
